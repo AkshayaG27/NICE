01529b40a17c70e704ab7ee3db2255c8d0ed1828
AkshayaG27 <ayahworld27@gmail.com>
2026-04-02 00:36:17 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -291,7 +291,7 @@ def generate_samples(model, prior, dataset_name, n_samples=100):
     nvis = NICE.PRESETS[dataset_name]['nvis']
 
     with torch.no_grad():
-        z = (prior.sample(n_samples, nvis)).to(device)    #added 0.7 to reduce temperature -> 
+        z = (prior.sample(n_samples, nvis)*0.7).to(device)    #added 0.7 to reduce temperature 
         x = model.decode(z)
         
     x = x.cpu()
